92774ac721fc909f2bcf7f3766f8779236789154
Alex <oleksandr.bodnarchuk@co3.eu>
2024-08-15 19:53:19 +0200

    Controller prep.

--- src/main/java/pl/alex/accounts/api/AccountsController.java
+++ src/main/java/pl/alex/accounts/api/AccountsController.java
@@ -1,5 +1,6 @@
 package pl.alex.accounts.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/accounts")
 public class AccountsController {
 
+  @Value("${server.port}")
+  private String serverPort;
+
   @GetMapping("/test")
   public String test() {
-    return "Hello from [accounts-service]";
+    return "Hello from [accounts-service] at port: " + serverPort;
   }
 }
